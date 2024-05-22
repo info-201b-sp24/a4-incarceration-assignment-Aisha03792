5208a9288e19648a6e18a456ea51f12d7b25b302
Aisha03972 <orcadolfin2005@gmail.com>
2024-05-22 11:45:41 -0700

    Updated hehe!

--- My Chart 1.R
+++ My Chart 1.R
@@ -1,1 +1,15 @@
-incarceration_trends <- read.csv("https://raw.githubusercontent.com/vera-institute/incarceration-trends/master/incarceration_trends.csv")
+
+library(ggplot2)
+
+# Imported the data and it's location
+
+prison_per_county_state <- read.csv("https://raw.githubusercontent.com/melaniewalsh/Neat-Datasets/main/us-prison-pop.csv")
+
+# Chart 1 is a line chart
+
+prison_per_state <- prison_per_county_state %>% 
+  group_by(state) %>%
+  summarize(total_prison_pop = sum(total_pop))
+
+  
+  
