cffceccd719dfb0d06b84af2eaf3d6e21b452337
Aisha03972 <orcadolfin2005@gmail.com>
2024-05-22 13:43:53 -0700

    Updated

--- My Chart 1.R
+++ My Chart 1.R
@@ -1,15 +1,12 @@
+# Trends over time chart
+# The first chart that you will create and include will show the trend over time
+# of your variable/topic. Think carefully about what you want to communicate to 
+# your user (you may have to find relevant trends in the dataset first!). 
+# Here are some requirements to help guide your design. 
 
 library(ggplot2)
 
-# Imported the data and it's location
+#Load the dataset
 
-prison_per_county_state <- read.csv("https://raw.githubusercontent.com/melaniewalsh/Neat-Datasets/main/us-prison-pop.csv")
+trends_over_time <- read.csv("incarceration_trends_jail_jurisdiction.csv")
 
-# Chart 1 is a line chart
-
-prison_per_state <- prison_per_county_state %>% 
-  group_by(state) %>%
-  summarize(total_prison_pop = sum(total_pop))
-
-  
-  
