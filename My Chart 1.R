fd9b73bf29d38bc8d239a87262db6f2dfbd72160
Aisha03972 <orcadolfin2005@gmail.com>
2024-05-23 01:08:49 -0700

    Updated

--- My Chart 1.R
+++ My Chart 1.R
@@ -1,12 +1,26 @@
-# Trends over time chart
-# The first chart that you will create and include will show the trend over time
-# of your variable/topic. Think carefully about what you want to communicate to 
-# your user (you may have to find relevant trends in the dataset first!). 
-# Here are some requirements to help guide your design. 
 
+# add important code 
 library(ggplot2)
 
-#Load the dataset
+# load dataset
 
-trends_over_time <- read.csv("incarceration_trends_jail_jurisdiction.csv")
+trends_over_time <- read.csv("https://raw.githubusercontent.com/vera-institute/incarceration-trends/master/incarceration_trends.csv")
+
+# Filtered data for black male prison population in California
+hehe <- subset(trends_over_time, state %in% c("WA", "TX", "CA"))
+
+filtered_data <- hehe[, c("year", "state", "aapi_pop_15to64", "black_pop_15to64", "native_pop_15to64", "latinx_pop_15to64")]
+
+
+# work site
+ggplot(data = filtered_data, aes(
+  x = year, 
+  y =  black_pop_15to64,
+  color = state)) +
+  geom_line(na.rm = FALSE) + 
+  labs(y = "Black population", 
+       x = "Years",
+       color = "Population Group") +
+  ggtitle("Black Population in jail across different states over time!")
+
 
