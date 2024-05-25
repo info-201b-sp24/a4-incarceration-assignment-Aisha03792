543b0dba4e52ea731c6117701f5f8b38510e4953
Aisha03972 <orcadolfin2005@gmail.com>
2024-05-25 04:02:36 -0700

    updated

--- My Chart 1.R
+++ My Chart 1.R
@@ -12,21 +12,13 @@ years_filtered <- subset(new_data, year %in% c("2000", "2005", "2010","2015"))
 # Filtering for the races being compared!
 
 states_filtered <- subset(years_filtered, state %in% c("TX", "CA", "WA"))
-  
+
 all_filtered <- select(states_filtered, c("year","state","black_jail_pop","latinx_jail_pop", "native_jail_pop"))
 
-# work site
-ggplot(data = states_filtered, aes(
-  x = year, 
-  group = state)) +
-  geom_bar(aes(y = black_jail_pop, fill = "Black Jail Pop"), stat = "identity", position = "dodge") + 
-  geom_bar(aes(y = latinx_jail_pop, fill = "Latinx Jail Pop"), stat = "identity", position = "dodge") +
-  labs(y = "Total Population ranges", 
-       x = "Years",
-       fill = "Population") +
-  ggtitle("Comparison of Black and Latinx Jail Population in Different States over the years!") +
-  scale_fill_manual(values = c("Black Jail Pop" = "blue",
-                               "Latinx Jail Pop" = "red"),
-                    name = "Population",
-                    breaks = c("Black Jail Pop", "Latinx Jail Pop")) +
-  facet_wrap(~state)
+#bar chart over time 
+ggplot(all_filtered, aes(x = year, y = black_jail_pop, fill = state)) + 
+  geom_bar(stat = "identity", position = "dodge") + 
+  labs(x = "Year", 
+       y = "Population",
+       title = "Black Jail Population Across Different States") + 
+  theme_minimal()
--- My Chart 2.R
+++ My Chart 2.R
@@ -1,3 +1,4 @@
+
 # add important code 
 library(ggplot2)
 library(dplyr)
@@ -15,6 +16,18 @@ states_filtered <- subset(years_filtered, state %in% c("TX", "CA", "WA"))
 
 all_filtered <- select(states_filtered, c("year","state","black_jail_pop","latinx_jail_pop", "native_jail_pop"))
 
-
-
-
+# work site
+ggplot(data = states_filtered, aes(
+  x = year, 
+  group = state)) +
+  geom_bar(aes(y = black_jail_pop, fill = "Black Jail Pop"), stat = "identity", position = "dodge") + 
+  geom_bar(aes(y = latinx_jail_pop, fill = "Latinx Jail Pop"), stat = "identity", position = "dodge") +
+  labs(y = "Total Population ranges", 
+       x = "Years",
+       fill = "Population") +
+  ggtitle("Comparison of Black and Latinx Jail Population in Different States over the years!") +
+  scale_fill_manual(values = c("Black Jail Pop" = "blue",
+                               "Latinx Jail Pop" = "red"),
+                    name = "Population",
+                    breaks = c("Black Jail Pop", "Latinx Jail Pop")) +
+  facet_wrap(~state)
