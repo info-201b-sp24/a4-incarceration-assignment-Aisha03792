65bb903fbb07dcb4b0aa66f80dc285c0b9dc1f34
Aisha03972 <orcadolfin2005@gmail.com>
2024-05-26 22:53:40 -0700

    latest update

--- My Map.R
+++ My Map.R
@@ -9,26 +9,23 @@ new_data <- read.csv("incarceration_trends.csv")
 # Filtering for the races being compared and state (California)
 filtering_data_map <- new_data %>% 
   filter(state == "WA") %>% 
-  select("state","county_name", "latinx_jail_pop", "black_jail_pop")
+  select("state","county_name", "latinx_jail_pop")
 
 
-max_black_jail_pop <- max(filtering_data$black_jail_pop, na.rm = TRUE)
+max_black_jail_pop <- max(filtering_data_map$latinx_jail_pop, na.rm = TRUE)
 
-new_data <- state(cb = TRUE)
 
-ca_shape <- map_data('county', 'california')
+ca_shape <- map_data('county', 'washington')
 
 # Joining shape data with population data
 state_shape <- left_join(ca_shape, filtering_data_map, by = c("subregion" = "county_name"))
 
 # Plotting
-ggplot(state_shape, aes(x = long, y = lat, group = group, fill = black_jail_pop)) +
+ggplot(state_shape, aes(x = long, y = lat, group = group, fill = latinx_jail_pop)) +
   geom_polygon(color = "black", size = 0.1) +
   coord_map() +
-  scale_fill_gradientn(colours = c("blue", "darkblue"), na.value = "lightyellow", limits = c(0, max_black_jail_pop)) +
-  labs(fill = "Black Jail Population",
-       title = "Distribution of Black Jail Population in California Counties") +
+  scale_fill_gradientn(colours = c("green", "darkgreen"), na.value = "skyblue", limits = c(0, max_black_jail_pop)) +
+  labs(fill = "Latinx Jail Population",
+       title = "Distribution of Latinx Jail Population in Washington State") +
   theme_void() 
 
-
-# Black prison population in California state's counties 
